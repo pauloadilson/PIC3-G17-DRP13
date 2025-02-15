3fee4b4560a892233c3c7dd0bff8e4e906fb779c
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2025-02-15 10:42:55 -0300

    Adiciona suporte para carregar variáveis de ambiente a partir do arquivo .env e atualiza .gitignore para incluir .vscode/

--- cpprev/settings.py
+++ cpprev/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import secrets
 import os
 import django_heroku
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
