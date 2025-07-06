70442da7446dbc1312421ebfbcd8fca72593ccb9
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2025-07-06 11:55:18 -0300

    Refactor GlobalDefaultPermission to use serializer_class for model name and app label retrieval

--- cpprev/permissions.py
+++ cpprev/permissions.py
@@ -31,11 +31,8 @@ class GlobalDefaultPermission(permissions.BasePermission):
 
     def __get_model_permission_codename(self, method, view):
         try:
-            model_name = view.queryset.model._meta.model_name
-            print(f"Model name: {model_name}")  # Debugging line
-            app_label = view.queryset.model._meta.app_label
-            print(f"App label: {app_label}")  # Debugging line
-            print(f"Method: {method}")  # Debugging line
+            model_name = view.serializer_class.Meta.model._meta.model_name
+            app_label = view.serializer_class.Meta.model._meta.app_label
             action = self.__get_action_sufix(method)
             return f'{app_label}.{action}_{model_name}'
         except AttributeError:
